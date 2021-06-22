0929c3a6ae98319d2e2e18ce6deae5e532791f67
TuanThai <minhtuanthaivn@gmail.com>
2021-06-22 22:10:09 +0700

    update database url

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const url = "mongodb://localhost:27017/EmployeeDB";
-
+//const url = "mongodb://localhost:27017/EmployeeDB";
+const url = "mongodb+srv://tmtuan:tmtuan@cluster0.zfovn.mongodb.net/test"
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
